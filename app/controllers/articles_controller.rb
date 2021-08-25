4b14650d8dfc9a6588ba2730b2fe00399be52057
Muhammad Ibraheem <ibrahimaziz719@gmail.com>
2021-08-25 14:16:15 +0500

    add edit, update,show and idex listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,20 @@
 class ArticlesController < ApplicationController
 
+def index
+@articles = Article.all
+end
+
+
+
+
 def new
   @article = Article.new
 end
+
+def edit
+@article = Article.find(params[:id])
+end
+
 
 def create
   @article = Article.new(article_params)
@@ -12,8 +24,20 @@ redirect_to article_path(@article)
 
 else
   render 'new'
+
 end
 end
+
+def update
+@article = Article.find(params[:id])
+if @article.update(article_params)
+  flash[:notice] = "article was successfuly updated"
+  redirect_to article_path(@article)
+else
+  render 'edit'
+end
+end
+
 
 
 def show
@@ -25,6 +49,4 @@ private
 def article_params
   params.require(:article).permit(:title, :description)
 end
-
-
 end
