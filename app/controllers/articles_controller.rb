685158fcc2869edc19abd5d49cf28a81e2b42f30
Muhammad Ibraheem <ibrahimaziz719@gmail.com>
2021-09-13 15:50:17 +0500

    added restric actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ end
 
 def create
   @article = Article.new(article_params)
-  @article.user = User.first
+  @article.user = current_user
 if @article.save
   flash[:success]= "article was successfully created"
 redirect_to article_path(@article)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
 def index
 @users =  User.paginate(page: params[:page], per_page: 3)
 end
@@ -8,7 +9,7 @@ def new
 end
 
 def create
-@user = User.new(user_params)
+  @user = User.new(user_params)
 if @user.save
 flash[:success] = "Welcome to the Alpoha Blog #{@user.username}"
 redirect_to articles_path
@@ -19,13 +20,10 @@ end
 
 
 def edit
-@user = User.find(params[:id])
-
 end
 
 
 def update
-@user = User.find(params[:id])
 if @user.update(user_params)
   flash[:success]= "Your account was updated successfuly"
   redirect_to articles_path
@@ -37,7 +35,6 @@ end
 
 
 def show
-@user = User.find(params[:id])
 @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 end
 
@@ -47,5 +44,7 @@ def user_params
 params.require(:user).permit(:username,  :email, :password)
 end
 
-
+def set_user
+@user = User.find(params[:id])
 end
+end 
