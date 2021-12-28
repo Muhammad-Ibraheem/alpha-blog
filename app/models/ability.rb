dc90ee4a9059d67a85888232e7a21f16f431ebf5
ibraheem <ibrahinaziz719@gmail.com>
2021-12-28 13:38:33 +0500

    specify roles for admin,author and reader

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,24 +5,9 @@ class Ability
       if user.has_role?(:admin)
         can :manage, :all
       else
-        can :read, :all
-      end
+        can :manage, Article if user.has_role?(:author, Article)
+        can :read, :Article
 
-    # The first argument to `can` is the action you are giving the user 
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. 
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    #https://github.com/ryanb/cancan/wiki/Defining-Abilities
+      end
   end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,14 +1,15 @@
 class Role < ApplicationRecord
   has_and_belongs_to_many :users, :join_table => :users_roles
-  
+  enum name: {admin: 'admin', author: 'author', reader: 'reader'}
+
+
   belongs_to :resource,
              :polymorphic => true,
              :optional => true
-  
 
   validates :resource_type,
             :inclusion => { :in => Rolify.resource_types },
-            :allow_nil => false
+            :allow_nil => true
 
   scopify
 end
